7892a0b1cde51e887c8959d4dd0ab5d628e969b8
Juan Cruz Olivera <47878970+jcruzolivera@users.noreply.github.com>
2024-07-29 12:25:34 -0300

    Initial commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.emojiName}</span>
+      </dt>
+      <dd>{props.emojiDescription}</dd>
+    </div>
+  );
+}
+
+export default Entry;
